5b3ddde21c0ae767186f4553ff392da579e559a1
Linimik <jameskimimk@gmail.com>
2022-04-02 17:31:35 -0400

    Disabled jekyll

--- build.ts
+++ build.ts
@@ -1,6 +1,8 @@
+import fs from "fs"
 import { publish } from "gh-pages"
 
+fs.writeFileSync("./build/.nojekyll", "")
 publish("build", (error) => {
-	if (error) return console.error(error);
+	if (error) return console.error(error)
 	console.log("Built!")
 })
