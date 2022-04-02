6935f8dac6a6dc342f52a0cac5070e4ff7574ba5
Linimik <jameskimimk@gmail.com>
2022-04-02 17:14:02 -0400

    Added auto toggle dev

---
+++ toggleDev.ts
@@ -0,0 +1,14 @@
+import fs from "fs"
+
+const arg = process.argv[2]
+console.log(arg)
+
+const mode = arg === "true" || arg === "false" ? arg : "true"
+
+const code = `export const dev = ${mode}
+export const basePath = "/simple-game-maker"
+export const fullBasePath = "https://jameslinimk.github.io/simple-game-maker/"
+`
+
+fs.writeFileSync("./src/lib/conf.js", code)
+console.log("Done!")
