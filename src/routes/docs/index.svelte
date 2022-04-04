a66f4ab53273fad6a3e3501fe61cd5839c180869
Linimik <jameskimimk@gmail.com>
2022-04-03 23:57:25 -0400

    Index doesnt work as ts file

---
+++ src/routes/docs/index.svelte
@@ -0,0 +1,8 @@
+<script context="module" lang="ts">
+	import { parseHref } from "$lib/conf"
+
+	export const load = () => ({
+		status: 302,
+		redirect: parseHref("/docs/home")
+	})
+</script>
--- src/routes/docs/index.ts
+++
@@ -1,6 +0,0 @@
-import { parseHref } from "$lib/conf"
-
-export const get = async () => ({
-	headers: { Location: parseHref("/docs/home") },
-	status: 302
-})
