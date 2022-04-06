ea4e0625e6d78e808ccc93f4415321b312d73718
Linimik <jameskimimk@gmail.com>
2022-04-05 23:34:41 -0400

    Fixed HREFs (again?)

--- src/lib/conf.js
+++ src/lib/conf.js
@@ -6,4 +6,4 @@ export const baseURL = "https://jameslinimk.github.io/simple-game-maker"
  * @param {string} path
  * @returns {string} new path
  */
-export const parseHref = (path) => (dev ? path : `${basePath}${path}`)
+export const parseHref = (path) => path
