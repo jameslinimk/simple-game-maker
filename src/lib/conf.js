48aca7c70d000d33c5a54cd20e7ec66717e44a90
Linimik <jameskimimk@gmail.com>
2022-04-05 23:29:21 -0400

    Forgot to change HREFs

--- src/lib/conf.js
+++ src/lib/conf.js
@@ -6,4 +6,6 @@ export const baseURL = "https://jameslinimk.github.io/simple-game-maker"
  * @param {string} path
  * @returns {string} new path
  */
-export const parseHref = (path) => (dev ? path : `${basePath}${path}`)
+export const parseHref = (path) => path
+
+// FIXME Shits useless now of netlify but I spent too much time on this I can't
